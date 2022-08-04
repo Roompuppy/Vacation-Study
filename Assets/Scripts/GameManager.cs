6ed70b6bdcf5b40ebf88e8a51e0c975699c7dd91
Roompuppy <86652628+Roompuppy@users.noreply.github.com>
2022-08-04 11:16:04 +0900

    'ClassCoin' GameObjects Setting 8.4
    Player 및 Coin 생성 및 상호작용 일부 적용

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject coin;
+
+    void start()
+    {
+        CapsuleCollider col = coin.GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody rbody;
+    public Vector3 moveDir;
+    public float moveSpeed;
+
+    void Update()
+    {
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveDir.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + moveDir*moveSpeed);
+    }
+}
